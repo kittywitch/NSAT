922815942d27b25881753eaf46034ea6dc7ad97a
kat witch <kat@kittywit.ch>
2019-04-30 16:47:55 +0100

    Added notifications on successful and non-successful connections

--- server/modules/client_auth.py
+++ server/modules/client_auth.py
@@ -6,7 +6,7 @@ import core
 @core.add_action("register")
 def register_client(data, server):
 	# This implements the two actions, enroll_token and reject and their JSON structures.
-	peer = "%s:%s" % (server._peer.host, server._peer.port)
+	peer = f"{server._peer.host}:{server._peer.port}"
 	user_input = input(f"Allow registration for client {peer} (yes/no)? ")
 	if user_input == "yes":
 		core.socket_send(server, json.dumps({
@@ -14,29 +14,43 @@ def register_client(data, server):
 			"uuid":data["uuid"],
 			"token":data["token"]
 		}))
-		
+		output = f"Registered token UUID \"{data['uuid']}\" with token \"{data['token']}\" for peer \"{peer}\"."
+		handlers.pushover.notify(output)
+		handlers.sms.notify_sms(output)
+		logging.info(output)
 		core.add_token(data["uuid"], data["token"])
-		logging.info(f"Registered client for {peer}.")
 	elif user_input == "no":
 		core.socket_send(server, json.dumps({
 			"action":"reject"
 		}))
-		logging.error("User not allowed to register.")
+		output = f"Peer \"{peer}\" not allowed to register with UUID \"{data['uuid']}\" and token \"{data['token']}\"."
+		handlers.pushover.notify(output)
+		handlers.sms.notify_sms(output)
+		logging.error(output)
 
 
 @core.add_action("reestablish")
 def reestablish_client(data, server):
 	# This allows a client that already has enrolled a token to reestablish a tokenized connection.
 	#peer = "%s:%s" % (server._peer.host, server._peer.port)
-	peer = data["uuid"]
+	peer = f"{server._peer.host}:{server._peer.port}"
+	uuid = data["uuid"]
 	token = data["token"]
-	if peer in core.token_db:
-		if core.token_db[peer] == token:
-			logging.info("Token valid for peer.")
-			core.socket_send(server, json.dumps({
+	if uuid in core.token_db:
+		if core.token_db[uuid] == token:
+				output = f"UUID \"{uuid}\" and token \"{token}\" valid for peer \"{peer}\"."
+				handlers.pushover.notify(output)
+				handlers.sms.notify_sms(output)
+				logging.info(output)
+				core.socket_send(server, json.dumps({
 				"action":"authorised"
 			}))
 		else:
-			logging.error("Token not valid for peer.")
+			output = f"UUID \"{uuid}\" and token \"{token}\" are not valid for peer \"{peer}\"."
+			handlers.pushover.notify(output)
+			handlers.sms.notify_sms(output)
+			logging.error(output)
 	else:
-		logging.error("Peer not established.")
+		handlers.pushover.notify(output)
+		handlers.sms.notify_sms(output)
+		logging.error(output)
